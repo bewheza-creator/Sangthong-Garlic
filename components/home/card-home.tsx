c9f53744867a7440e49cab492440b8fa6bfbbcb8
Faris4166 <farisarthan@gmail.com>
2026-08-04 23:05:04 +0700

    แก้ไขลิงค์

--- components/home/card-home.tsx
+++ components/home/card-home.tsx
@@ -44,7 +44,7 @@ const data = [
     name: "อื่นๆ",
     details: "หอมเจียวราคาส่ง พริกไทราคาส่ง | ร้านแสงทอง ตลาดไท",
     img: "/images/home/home4.png",
-    link: "/product/b67e417b-f373-4a9a-a4a9-39096134a6be",
+    link: "/product/18fe19a0-fd84-4fd8-8d4c-b53fe202259f",
     accent: {
       card: "bg-[#d6e5ed]",
       title: "text-[#1676a0]",
