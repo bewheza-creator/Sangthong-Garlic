a0a2b9d32c3cc87ab47af4c8d0d85ba543ea2706
bewheza-creator <bewheza@gmail.com>
2026-08-12 00:30:38 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://sangthong-garlic.vercel.app/sitemap.xml',
+    sitemap: 'https://sangthong-garlic.com/sitemap.xml',
   }
 }
